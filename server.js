c2f71770318a246a63aa4518f7d22d0ca0378efa
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2021-01-03 17:16:43 +0100

    Add first route

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const products = require("./data/products");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  //   step by step
+  //   1. step by step
   //   res.statusCode = 200;
   //   res.setHeader("Content-Type", "text/html");
   //   res.write("<h1>Hello World</h1>");
   //   res.end();
 
-  //   shorthand
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-  res.end(JSON.stringify(products));
+  //   2. shorthand
+  //   res.writeHead(200, {
+  //     "Content-Type": "application/json",
+  //   });
+  //   res.end(JSON.stringify(products));
+
+  //   3. with routing
+  if (req.url === "/api/products") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify(products));
+  }
 });
 
 server.listen(PORT, () => {
